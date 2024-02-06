6808db6e5aec525ce33c5517e3b9165b09c3a11d
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2024-02-06 00:58:07 -0300

    finish homepage

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
     return (
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
             <span className="text-sm font-medium text-slate-300">
                 há 8 dias
             </span>
@@ -8,6 +8,6 @@ export function NoteCard() {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis esse itaque velit at tempore quam excepturi impedit, non adipisci, quasi totam, accusamus ad repudiandae. Necessitatibus itaque tenetur ipsum architecto recusandae?
             </p>
             <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
+        </button>
     )
 }
