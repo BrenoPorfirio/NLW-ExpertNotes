3e4bc0130c2ceacff3ad0d04d52543466200e3c6
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2024-02-06 00:47:44 -0300

    finishing card

--- src/app.tsx
+++ src/app.tsx
@@ -15,16 +15,46 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 ">
-
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-200">
+            Adicionar nota
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Grave uma nota em áudio que será convertida para texto automaticamente.
+          </p>
         </div>
 
-        <div className="rounded-md bg-slate-800 ">
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">
+            há 2 dias
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae earum, veritatis, doloribus similique harum aliquam expedita accusamus ipsum, eligendi recusandae modi. Culpa nihil suscipit adipisci corrupti eaque officiis tenetur beatae!
+          </p>
 
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
         </div>
 
-        <div className="rounded-md bg-slate-800 ">
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">
+            há 4 dias
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis esse itaque velit at tempore quam excepturi impedit, non adipisci, quasi totam, accusamus ad repudiandae. Necessitatibus itaque tenetur ipsum architecto recusandae?
+          </p>
 
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">
+            há 8 dias
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis esse itaque velit at tempore quam excepturi impedit, non adipisci, quasi totam, accusamus ad repudiandae. Necessitatibus itaque tenetur ipsum architecto recusandae?
+          </p>
+
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
         </div>
       </div>
     </div>
