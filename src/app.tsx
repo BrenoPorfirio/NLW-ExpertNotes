d4b5c5d871ddea9a245535ff869baa70068294d9
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2024-02-06 00:18:35 -0300

    add separator

--- src/app.tsx
+++ src/app.tsx
@@ -2,15 +2,17 @@ import logo from './assets/logo-nlw-expert.svg'
 
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 ">
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
 
-      <form className="w-full">
+      <form className="w-full mt">
         <input type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
         />
       </form>
+
+      <div className="h-px bg-slate-700" />
     </div>
   )
 }
